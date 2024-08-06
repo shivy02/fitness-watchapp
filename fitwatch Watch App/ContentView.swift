42e2d102305687e5883841a51c2dac1bf6ee6a3d
Shivam Patel <smpatel0423@gmail.com>
2024-08-06 01:43:03 -0400

    Starting project

---
+++ fitwatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fitwatch Watch App
+//
+//  Created by Shivam Patel on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fitwatch Watch App/fitwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitwatchApp.swift
+//  fitwatch Watch App
+//
+//  Created by Shivam Patel on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct fitwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
